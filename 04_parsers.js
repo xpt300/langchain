8cc145337dbe184bb922db27d3bb11b7e661ad41
Maxime Joubert <maxime.joubert@needhelp.com>
2024-03-06 17:26:48 +0100

    feqt: 04_parser work well

--- 04_parsers.js
+++ 04_parsers.js
@@ -1,31 +1,29 @@
 import { config } from "dotenv";
 config();
 
-import { OpenAI } from "langchain/llms/openai";
-import { PromptTemplate } from "langchain/prompts";
-import { StructuredOutputParser } from "langchain/output_parsers";
+import { OpenAI } from "@langchain/openai";
+import {
+  StructuredOutputParser,
+  RegexParser,
+  CombiningOutputParser,
+} from "langchain/output_parsers";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { RunnableSequence } from "@langchain/core/runnables";
 
-const parser = StructuredOutputParser.fromNamesAndDescriptions({
-  answer: "answer to the user's question",
-});
-const formatInstructions = parser.getFormatInstructions();
-
-const prompt = new PromptTemplate({
-  template:
-    "Be very funny when answering questions\n{format_instructions}\n Question: {question}",
-  inputVariables: ["question"],
-  partialVariables: { format_instructions: formatInstructions },
+const answerParser = StructuredOutputParser.fromNamesAndDescriptions({
+  answer: "Is there a phone number or address or company name on this question?",
+  source: "source used to answer the user's question, should be a phone number, address or company name context.",
 });
 
-const model = new OpenAI({ temperature: 0 });
 
-const input = await prompt.format({
-  question: "What is the capital of France?",
+const chain = RunnableSequence.from([
+  PromptTemplate.fromTemplate(
+    "Answer the users question as a boolean format.\n{format_instructions}\n{question}"
+  ),
+  new OpenAI({ temperature: 0 }),
+]);
+
+const response = await chain.invoke({
+  question: "Hello i'm a bulder and i'm happy to speak with you, Menuiserie Paris",
+  format_instructions: answerParser.getFormatInstructions(),
 });
-console.log(input);
-
-const response = await model.call(input);
-
-console.log(response);
-
-console.log(await parser.parse(response));
